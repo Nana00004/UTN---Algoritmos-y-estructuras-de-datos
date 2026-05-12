173271938646d4d8cf7818d5498204a3a1b3b1fd
Morena Godoy <190492571+Nana00004@users.noreply.github.com>
2026-05-12 13:12:59 -0300

    Delete conversion.py

--- conversion.py
+++
@@ -1,8 +0,0 @@
-tiempo_total = int(input("Ingrese el tiempo transcurrido (en segundos): "))
-
-horas = tiempo_total // 3600
-remanente = tiempo_total % 3600
-minutos = remanente // 60
-segundos = remanente % 60
-
-print("Horas:", horas, ":", minutos, ":", segundos)
